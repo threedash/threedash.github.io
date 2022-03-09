b9ef611aceb6da8915fd99710cab2fe76388d11f
gby.su <imansiberia@icloud.com>
2022-03-09 22:32:32 +0600

    without theme change

--- src/app/components/game-info/game-info.component.ts
+++ src/app/components/game-info/game-info.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute} from '@angular/router';
 import {DataService} from '../../services/data.service';
 import {ICompetition, IGame} from '../../interfaces/itable';
 import {MatTableDataSource} from '@angular/material/table';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-game-info',
@@ -12,6 +13,14 @@ import {MatTableDataSource} from '@angular/material/table';
 export class GameInfoComponent implements OnInit {
   displayedColumns: string[] = [];
   dataSource: MatTableDataSource<IGame>;
+  isActive = {
+    topWinning: true,
+    notSoWinning: false,
+    mostAppearances: false
+  };
+  sortOption = 'desc';
+  youTubeLink: SafeUrl = '';
+
   set data(value: ICompetition) {
     this._data = value;
   }
@@ -28,11 +37,13 @@ export class GameInfoComponent implements OnInit {
     game: [],
     total: undefined,
   };
+  iframeHtml: any;
 
 
   constructor(
     private route: ActivatedRoute,
     private dataService: DataService,
+    private sanitizer: DomSanitizer
   ) {
   }
 
@@ -44,14 +55,51 @@ export class GameInfoComponent implements OnInit {
 
   private getGameInfo(id: string) {
     this.dataService.getGameInfo(id).subscribe((data) => {
+      console.log(data);
       this.data = data;
-      this.getTable();
+      this.drawTable();
+      this.getYouTubeLink();
     });
 
   }
 
-  private getTable() {
+  private drawTable() {
     this.dataSource = new MatTableDataSource<IGame>(this.data.game);
     this.displayedColumns = ['index', 'player', 'result'];
+  }
+
+  sortTable(name: string) {
+    switch (name) {
+      case 'win':
+        this.isActive.topWinning = true;
+        this.isActive.notSoWinning = false;
+        this.sortOption = 'desc';
+        this.sortData();
+        break;
+      case 'loose':
+        this.isActive.topWinning = false;
+        this.isActive.notSoWinning = true;
+        this.sortOption = 'asc';
+        this.sortData();
+        break;
+      default:
+        break;
+    }
+  }
+
+  private sortData() {
+    if (this.sortOption === 'asc') {
+      this.data.game.sort((a, b) => +a.result - +b.result);
+    } else if (this.sortOption === 'desc') {
+      this.data.game.sort((a, b) => +b.result - +a.result);
+    }
+    this.drawTable();
+  }
+
+  private getYouTubeLink() {
+    const template = '<iframe id="ytplayer" width="100%" height="360" [src]="youTubeLink"></iframe>';
+    const tempLink = 'https://www.youtube.com/embed/' + this.data.yLink.split('=')[1].split('&')[0] + '?autoplay=1';
+    this.youTubeLink = this.sanitizer.bypassSecurityTrustResourceUrl(tempLink);
+    console.log(this.youTubeLink);
   }
 }
